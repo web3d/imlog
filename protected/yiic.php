a6ee78c4ae59a1003e1ba884eff611e8cb27a699
web3d <web3d@live.com>
2012-07-30 13:46:11 +0800

    [Add] 初始化

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../Framework/yii-1.1.11.58da45/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
